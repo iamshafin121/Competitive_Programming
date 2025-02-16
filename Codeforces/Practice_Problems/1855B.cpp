1453cb1e6d5b86618338f50187dcdac5b74308e0
iamshafin121 <ahamedshafin121@gmail.com>
2025-02-16 15:14:54 +0600

    1855B solved

---
+++ Codeforces/Practice_Problems/1855B.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+/*
+       _______ ___  ___ ____   ______ _______ __   ___
+      /  ____//  / /  // _  | / ____//_  ___//  | /  /
+     (___   )/  /_/  // /_| |/ /___   / /   /   |/  /
+    ____/  //  __   // ___  / ____/__/ /__ /  | |  /
+   /______//__/ /__//_/  |_/_/   /_______//__/| __/
+                                                  
+*/
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        ll count= 0;
+        for(ll i=1; 1 ; i++){
+            if(n%i != 0) break;
+            count++;
+        }
+        cout << count << endl;
+    }
+    
+    return 0;
+}
