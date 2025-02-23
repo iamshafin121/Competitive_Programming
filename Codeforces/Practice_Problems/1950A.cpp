88de5a47591ada7029ee64bec2dd3e36d092f05d
iamshafin121 <ahamedshafin121@gmail.com>
2025-02-23 18:55:38 +0600

    1950A solved

---
+++ Codeforces/Practice_Problems/1950A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+/*
+       _______ ___  ___ ____   ______ _______ __   ___
+      /  ____//  / /  // _  | / ____//_  ___//  | /  /
+     (___   )/  /_/  // /_| |/ /___   / /   /   |/  /
+    ____/  //  __   // ___  / ____/__/ /__ /  | |  /
+   /______//__/ /__//_/  |_/_/   /_______//__/| __/
+                                                  
+*/
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t; cin >> t;
+    while(t--){
+        ll a, b, c; cin >> a >> b >> c;
+        if(a < b && b < c)
+            cout << "STAIR" << endl;
+        else if(a < b && b > c)
+            cout << "PEAK" << endl;
+        else
+            cout << "NONE" << endl;
+    }
+    
+    return 0;
+}
