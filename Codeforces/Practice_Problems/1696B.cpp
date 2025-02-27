0e636f0be1f7108bb6d2ac3bc4d22c68f85d2ca2
iamshafin121 <ahamedshafin121@gmail.com>
2025-02-27 15:52:42 +0600

    1696B solved

---
+++ Codeforces/Practice_Problems/1696B.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+/*
+       _______ ___  ___ ____   ______ _______ __   ___
+      /  ____//  / /  // _  | / ____//_  ___//  | /  /
+     (___   )/  /_/  // /_| |/ /___   / /   /   |/  /
+    ____/  //  __   // ___  / ____/__/ /__ /  | |  /
+   /______//__/ /__//_/  |_/_/   /_______//__/| __/
+                                                  
+*/
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        vector<ll> v(n); for(auto &ele : v) cin >> ele;
+        
+        bool greater = false;
+        bool after = false;
+        ll count = 0;
+        ll zero_cnt = 0;
+
+        for(ll i=0; i<v.size(); i++){
+            if(v[i] > 0) greater = true;
+            if(greater && v[i] == 0) after = true;
+            if(greater && after && v[i] > 0) count = 2;
+            if(v[i] == 0) zero_cnt++;
+        }
+
+        if(count == 2) cout << count << endl;
+        else{
+            if(v.size() == zero_cnt) cout << 0 << endl;
+            else cout << 1 << endl;
+        }
+    }
+    
+    return 0;
+}
