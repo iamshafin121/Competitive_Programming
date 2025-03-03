40e024c15b15daebad382f177d520115511b041a
iamshafin121 <ahamedshafin121@gmail.com>
2025-03-03 15:21:39 +0600

    1896A solved

---
+++ Codeforces/Practice_Problems/1896A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+/*
+       _______ ___  ___ ____   ______ _______ __   ___
+      /  ____//  / /  // _  | / ____//_  ___//  | /  /
+     (___   )/  /_/  // /_| |/ /___   / /   /   |/  /
+    ____/  //  __   // ___  / ____/__/ /__ /  | |  /
+   /______//__/ /__//_/  |_/_/   /_______//__/| __/
+                                                  
+*/
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        vector<ll> v(n); for(auto &ele : v) cin >> ele;
+        if(v[0] == 1) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    
+    return 0;
+}
