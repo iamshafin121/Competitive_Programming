39d714b6274321cf41d0e23ad487b765333188ef
iamshafin121 <ahamedshafin121@gmail.com>
2025-03-09 15:56:59 +0600

    1722A solved

---
+++ Codeforces/Practice_Problems/1722A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+/*
+       _______ ___  ___ ____   ______ _______ __   ___
+      /  ____//  / /  // _  | / ____//_  ___//  | /  /
+     (___   )/  /_/  // /_| |/ /___   / /   /   |/  /
+    ____/  //  __   // ___  / ____/__/ /__ /  | |  /
+   /______//__/ /__//_/  |_/_/   /_______//__/| __/
+                                                  
+*/
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        string s; cin >> s;
+        if(s.size() == 5){
+            string x = "Timur";
+            bool flag = true;
+            for(auto ch : x){
+                if(s.find(ch) == string::npos){
+                    flag = false; break;
+                }
+            }
+            if(flag) cout << "YES" << endl;
+            else cout << "NO" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    
+    return 0;
+}
