477db775424deb7e6180e539be368906e71687dc
iamshafin121 <ahamedshafin121@gmail.com>
2025-04-17 23:48:51 +0600

    1985B solved

---
+++ Codeforces/Practice_Problems/1985B.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+/*
+       _______ ___  ___ ____   ______ _______ __   ___
+      /  ____//  / /  // _  | / ____//_  ___//  | /  /
+     (___   )/  /_/  // /_| |/ /___   / /   /   |/  /
+    ____/  //  __   // ___  / ____/__/ /__ /  | |  /
+   /______//__/ /__//_/  |_/_/   /_______//__/| __/
+                                                  
+*/
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+        if(n==3) cout << 3 << endl;
+        else cout << 2 << endl;
+    }
+    
+    return 0;
+}
