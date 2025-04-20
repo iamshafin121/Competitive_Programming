c946a4aaf3b6c25312a5cb976c237b1318278ffa
iamshafin121 <ahamedshafin121@gmail.com>
2025-04-20 23:18:10 +0600

    959A solved

---
+++ Codeforces/Practice_Problems/959A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+/*
+       _______ ___  ___ ____   ______ _______ __   ___
+      /  ____//  / /  // _  | / ____//_  ___//  | /  /
+     (___   )/  /_/  // /_| |/ /___   / /   /   |/  /
+    ____/  //  __   // ___  / ____/__/ /__ /  | |  /
+   /______//__/ /__//_/  |_/_/   /_______//__/| __/
+                                                  
+*/
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll n; cin >> n;
+    if(n&1){
+        cout << "Ehab" << endl;
+    }
+    else{
+        cout << "Mahmoud" << endl;
+    }
+    
+    return 0;
+}
