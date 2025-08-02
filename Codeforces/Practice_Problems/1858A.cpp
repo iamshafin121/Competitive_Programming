1f16313cfb761b5041e6d3af9eadd435c2c95372
iamshafin121 <ahamedshafin121@gmail.com>
2025-08-02 13:59:37 +0600

    1858A solved

--- Codeforces/Practice_Problems/1858A.cpp
+++ Codeforces/Practice_Problems/1858A.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
@@ -21,24 +20,18 @@ int main() {
     ll t;
     cin >> t;
     while (t--) {
-        ll n;
-        cin >> n;
-        vector<ll> v;
-        for (ll i = 0; i < n; i++) {
-            for (ll j = 1; j <= 4; j++) {
-                char c;
-                cin >> c;
-                if (c == '#') {
-                    v.push_back(j);
-                }
-            }
+        ll a, b, c;
+        cin >> a >> b >> c;
+        if (a > b)
+            cout << "First" << endl;
+        else if (b > a)
+            cout << "Second" << endl;
+        else {
+            if (c % 2)
+                cout << "First" << endl;
+            else
+                cout << "Second" << endl;
         }
-        reverse(v.begin(), v.end());
-
-        for (auto ele : v) {
-            cout << ele << " ";
-        }
-        cout << endl;
     }
 
     return 0;
